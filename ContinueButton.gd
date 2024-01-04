e38dc1164673b6fb5b04d79a869b1fa7734d54ac
Colton Donkersgoed <coltonmdonk@gmail.com>
2024-01-03 23:03:11 -0500

    implemented continue btn, camera

---
+++ ContinueButton.gd
@@ -0,0 +1,23 @@
+extends TextureButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_mouse_entered():
+	self.modulate = Color(1, 1, 1, 0.9) # Replace with function body.
+
+
+func _on_mouse_exited():
+	self.modulate = Color(1, 1, 1, 1) # Replace with function body.
+
+
+func _on_pressed():
+	pass # Replace with function body.
