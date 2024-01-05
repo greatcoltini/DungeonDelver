413f0a03059c7c4fe54caebd7ab7c38c02a48d37
Colton Donkersgoed <coltonmdonk@gmail.com>
2024-01-04 19:06:20 -0500

    work on loading data

--- game_manager.gd
+++ game_manager.gd
@@ -1,6 +1,7 @@
 extends Node
 
-
+# event instance
+var event_instance = preload("res://scenes/event_scene.tscn")
 
 var level;
 var risk = 0
@@ -9,8 +10,8 @@ var deck;
 
 var current_event
 
-const events = ["Loot"]
-const monster_events = ["Skeleton"]
+const events = ["loot"]
+const monster_events = ["skeleton"]
 
 var event_deck = []
 
@@ -19,10 +20,22 @@ var event_deck = []
 # room event generation
 func generate_new_room():
 	# if we have some level of risk, let's create chance for monsters
-	if risk >= 1:
+	if risk >= 0:
 		event_deck.append(monster_events.pick_random())
 	else:
 		event_deck.append(events.pick_random())
 	
 	current_event = event_deck.pick_random()
+	load_event(current_event)
 	
+	
+# function that loads the event and the json, applies the stuff
+func load_event(event):
+	var cur_event_instance = event_instance.instantiate()
+	
+	var json = JSON.new()
+	var json_string = "res://data/events/" + event + "-event.json"
+	var parse_res = json.parse(json_string)
+	var data = json.get_data()
+	
+	print(parse_res)
